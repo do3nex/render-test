6105e40cab56ae1639438afcfb6b5242fec58065
do3nex <johndoe.nex@proton.me>
2025-04-14 04:44:32 +0300

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -1,7 +1,7 @@
 const puppeteer = require("puppeteer");
 require("dotenv").config();
 
-const scrape = async (res) => {
+const scrape = async (req,res) => {
   const browser = await puppeteer.launch({
     args: [
       "--disable-setuid-sandbox",
